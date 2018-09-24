e57fe41e7e809ce92d16e809be5e976b7bef2f86
unknown <daniel@testim.io>
2018-09-24 14:42:28 +0300

    cmmit 1

---
+++ src/test/java/myclass.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class myclass {
+  @Test
+  public void f() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "D:\\seleniumfiles\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://yoniflenner.net/Xamples/pizza");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+  }
+}
